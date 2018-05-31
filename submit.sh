5b8a2baaeea13e142c1cc234a47b09cb140c497d
shouxing.sun <shouxing.sun@aliyun.com>
2018-05-31 14:36:06 +0800

    Backup for security.

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 storm jar _build/bsparse-0.0.1-SNAPSHOT-standalone.jar \
     org.apache.storm.flux.Flux \
-    --local --filter filters/baofu.properties \
+    --remote --filter filters/baofu.properties \
     topology.yaml
